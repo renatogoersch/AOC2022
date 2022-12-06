e5acabcd6ccff28e8e62cf7a8df5139f051fddd7
RENATOPARENTE-S\Renato Parente <renatop@sepiocyber.com>
2022-12-06 10:46:22 +0000

    Day 6

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -3,9 +3,12 @@ os.chdir(r"C:\Users\Renato Parente\Documents\AdventOfCode\AOC2022\Day6")
 with open('input_day6.csv') as file:
     input_day6 = [line.rstrip() for line in file]
 
-for i in range(3,len(input_day6[0])):
-    string = input_day6[0][i-3] + input_day6[0][i-2] + input_day6[0][i-1] + input_day6[0][i]
-    if len(set(string)) == 4:
-        print(i+3)
-        break
-    
+def get_result(block):
+    for i in range((block-1),len(input_day6[0])):
+        string = input_day6[0][i-(block-1):i+1]
+        if len(set(string)) == block:
+            print("Resultado para um word block de " + str(block) + " é: " + str(i+1))
+            break
+
+get_result(4)
+get_result(14)
