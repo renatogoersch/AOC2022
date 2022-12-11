dd1b15ee50fb6626a590b0aa8cbefe3a65297a95
Renato Parente <renatop@sepiocyber.com>
2022-12-11 18:37:12 +0000

    Day 11

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -10,62 +10,63 @@ ops = {
     '^' : operator.xor,
 }
 
-#with open('input_day11.csv') as file:
-with open('ti.csv') as file:
+with open('input_day11.csv') as file:
+#with open('ti.csv') as file:
     input_day11 = [line.rstrip() for line in file]
 
-sys.set_int_max_str_digits(1000000000)
-
 import time
+
+items = []
+
+class Item:
+    def __init__(self, owner):
+        self.owner = owner
+        self.name = len(items)+1
 
 class Monkeys:
     def __init__(self, position,start,monkey_operation,
         monkey_test,monkey_if1,monkey_if2):
         self.name = position
-        self.items = start
+        self.items = list(map(int, start))
         self.operation = monkey_operation
         self.test = (str(monkey_test[0]),int(monkey_test[1]))
         self.if1 = int(monkey_if1)
         self.if2 = int(monkey_if2)
         self.inspected = int(0)
 
-    def inspect(self,worry):
-        start_time2 = time.time()
+    def inspect(self):
         items = self.items.copy()
         for i in items:
-            print("1 --- %s seconds ---" % (time.time() - start_time2))
-            start_time2 = time.time()
             self.inspected += 1
-            #print("MACACO =", self.name, " ITEM =", i)
             worry = int(i)
-            #print(worry, self.operation[0],self.operation[1])
             if self.operation[1] == 'old':
-                print("ERROR", self.operation[0], worry)
                 worry = ops[self.operation[0]](worry,worry)
             else:
                 worry = ops[self.operation[0]](worry,
                     int(self.operation[1]))
-            if (time.time() - start_time) > 2.5:
-                #print(worry)
-                print(self.operation[0])
-                print(self.operation[1])
-            print("2 --- %s seconds ---" % (time.time() - start_time2))
-            start_time2 = time.time()
-            #print(" =", worry)
-            #worry = int(worry / 3)
-            #print(worry, "/ 3 =", worry)
-            if (worry % self.test[1]) > 5:
-                self.items.remove(i)
+            worry = int(worry/3)
+            if (worry % self.test[1]) > 0:
                 monkeys[self.if2].throw(worry)
-                #print(worry, "to monkey ", self.if2)
             else:
-                self.items.remove(i)
                 monkeys[self.if1].throw(worry)
-                #print(worry, "to monkey ", self.if1)
-            #print(" ")
-            print("3 --- %s seconds ---" % (time.time() - start_time2))
-            start_time2 = time.time()
-        
+        self.items = []
+
+    def inspect2(self,worry):
+        items = self.items.copy()
+        for worry in items:
+            self.inspected += 1
+            nworry = worry
+            nworry %= mod
+            if self.operation[1] == 'old':
+                nworry = ops[self.operation[0]](nworry,nworry)
+            else:
+                nworry = ops[self.operation[0]](nworry,
+                    int(self.operation[1]))
+            if (nworry % self.test[1]) == 0:
+                monkeys[self.if1].throw(nworry)
+            else:
+                monkeys[self.if2].throw(nworry)
+        self.items = []
 
     def throw(self,item):
         self.items.append(item)
@@ -85,22 +86,38 @@ def create_monkeys(monkeys,nmonkeys):
         monkeys.append(monkey)
     return monkeys
 
-nmonkeys = 4
+nmonkeys = 8
 monkeys = []
 monkeys = create_monkeys(monkeys,nmonkeys)
         
+    
+def hunt_monkeys(monkeys,nmonkeys):
+    for r in range(nmonkeys):
+        monkeys[r].inspect()
+
+rounds = 20
+for n in range(rounds):
+    hunt_monkeys(monkeys,nmonkeys)
+
+
+monkeys.sort(key=lambda x: x.inspected, reverse=True)
+print(monkeys[0].inspected * monkeys[1].inspected)
+
+nmonkeys = 8
+monkeys = []
+monkeys = create_monkeys(monkeys,nmonkeys)
+        
+mod = 1
+for monkey in monkeys:
+    mod *= int(monkey.test[1])
 worry = 0
 def hunt_monkeys(monkeys,nmonkeys,worry):
     for r in range(nmonkeys):
-        worry = monkeys[r].inspect(worry)
+        worry = monkeys[r].inspect2(worry)
 
-rounds = 156
+rounds = 10000
 for n in range(rounds):
-    #print(n)
-    start_time = time.time()
     hunt_monkeys(monkeys,nmonkeys,worry)
-    print(" ---------",n , " --- %s seconds ---" % (time.time() - start_time))
-#hunt_monkeys(monkeys,len(monkeys),worry)
 
-for n in monkeys:
-    print(n.name, n.inspected)
+monkeys.sort(key=lambda x: x.inspected, reverse=True)
+print(monkeys[0].inspected * monkeys[1].inspected)
